f021d43d1948dc1b15c3d298c215e062be9fa2be
zjulzq <kanysome@zju.edu.cn>
2015-01-06 00:36:41 +0800

    重命名变量。
    Signed-off-by: zjulzq <kanysome@zju.edu.cn>

--- src/main/java/cn/ctp/task/RestoreLogTask.java
+++ src/main/java/cn/ctp/task/RestoreLogTask.java
@@ -23,9 +23,9 @@ public class RestoreLogTask {
 	private String logFile;
 	private String mapFile;
 	private Set<String> basePackages = Sets.newHashSet();
-	private BiMap<String, String> classnames = HashBiMap.create();
-	private Table<String, String, String> methodnames = HashBasedTable.create();
-	private Table<String, String, String> fieldnames = HashBasedTable.create();
+	private BiMap<String, String> classNames = HashBiMap.create();
+	private Table<String, String, String> methodNames = HashBasedTable.create();
+	private Table<String, String, String> fieldNames = HashBasedTable.create();
 
 	public RestoreLogTask(String logFile, String mapFile, Set<String> basePackages) {
 		Preconditions.checkArgument(StringUtils.isNotEmpty(logFile), "the log file name can't be empty");
@@ -51,14 +51,14 @@ public class RestoreLogTask {
 					line = line.replaceAll(COLON, StringUtils.EMPTY);
 					List<String> list = splitter.splitToList(line);
 					latestClassName = list.get(0);
-					classnames.put(list.get(0), list.get(1));
+					classNames.put(list.get(0), list.get(1));
 				} else if (isMethodNameMap(line)) {
 					String temp = new String(line.substring(line.lastIndexOf(COLON) + 1));
 					List<String> list = splitter.splitToList(temp);
-					methodnames.put(latestClassName, list.get(0), list.get(1));
+					methodNames.put(latestClassName, list.get(0), list.get(1));
 				} else {
 					List<String> list = splitter.splitToList(line);
-					fieldnames.put(latestClassName, list.get(0), list.get(1));
+					fieldNames.put(latestClassName, list.get(0), list.get(1));
 				}
 			}
 		} catch (Exception e) {
@@ -89,8 +89,8 @@ public class RestoreLogTask {
 		try (BufferedReader reader = new BufferedReader(new FileReader(new File(logFile)));
 				BufferedWriter writer = new BufferedWriter(new FileWriter(new File(newLogFile)))) {
 			String line = null;
-			Set<String> badNames = classnames.values();
-			BiMap<String, String> bad2good = classnames.inverse();
+			Set<String> badNames = classNames.values();
+			BiMap<String, String> bad2good = classNames.inverse();
 			while ((line = reader.readLine()) != null) {
 				for (String badName : badNames) {
 					if (line.contains(badName)) {
